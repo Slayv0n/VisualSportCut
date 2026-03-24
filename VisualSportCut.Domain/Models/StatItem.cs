7c22f6a46876ff4d4c71dd818d44ce4ad613d666
Vain <dedpylqwer@gmail.com>
2026-03-24 20:25:14 +0300

    Reapply "Interfaces and new Model"
    This reverts commit 51ec28e4f70374fcec3a54c3013c4573e8d2cffd.

---
+++ VisualSportCut.Domain/Models/StatItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VisualSportCut.Domain.Models
+{
+    public class StatItem
+    {
+        public StatItem() { }
+        public string Category { get; private set; } = null!;
+        public int Count { get; private set; }
+        public string Color { get; private set; } = null!;
+
+        public static StatItem Create(string category, int count, string color)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(category);
+            ArgumentException.ThrowIfNullOrWhiteSpace(color);
+
+            var statItem = new StatItem
+            {
+                Category = category,
+                Count = count,
+                Color = color
+            };
+
+            return statItem;
+        }
+
+    }
+}
